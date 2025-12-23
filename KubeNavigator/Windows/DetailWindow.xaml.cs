ab1aac5039c12abf3c9d5dbb70e576d892f1e421
Felix Seidl <me@felixse.dev>
2025-12-23 13:42:32 +0100

    fix titlebar issues

--- KubeNavigator/Windows/DetailWindow.xaml.cs
+++ KubeNavigator/Windows/DetailWindow.xaml.cs
@@ -1,7 +1,9 @@
+using CommunityToolkit.WinUI.Helpers;
 using KubeNavigator.Pages;
 using KubeNavigator.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.UI;
 
 namespace KubeNavigator.Windows;
 
@@ -16,6 +18,8 @@ public sealed partial class DetailWindow : Window
         this.InitializeComponent();
         ExtendsContentIntoTitleBar = true;
         SetTitleBar(AppTitleBar);
+        AppTitleBar.Loaded += AppTitleBar_Loaded;
+        AppTitleBar.ActualThemeChanged += AppTitleBar_ActualThemeChanged;
 
         RootFrame.Navigate(typeof(DetailsWindowRootPage), viewModel);
     }
@@ -23,5 +27,44 @@ public sealed partial class DetailWindow : Window
     private void AppTitleBar_BackRequested(TitleBar sender, object args)
     {
         ViewModel.Details.GoBack();
+    }
+
+    private void AppTitleBar_Loaded(object sender, RoutedEventArgs e)
+    {
+        ApplySystemThemeToCaptionButtons(this);
+    }
+
+    private void AppTitleBar_ActualThemeChanged(FrameworkElement sender, object args)
+    {
+        ApplySystemThemeToCaptionButtons(this);
+    }
+
+    public void ApplySystemThemeToCaptionButtons(Window window)
+    {
+        var res = Application.Current.Resources;
+        Color buttonForegroundColor;
+        Color buttonHoverForegroundColor;
+
+        Color buttonHoverBackgroundColor;
+        if (ViewModel.App.ThemeManager.GetEffectiveTheme() == "dark")
+        {
+            buttonForegroundColor = "#FFFFFF".ToColor();
+            buttonHoverForegroundColor = "#FFFFFF".ToColor();
+
+            buttonHoverBackgroundColor = "#0FFFFFFF".ToColor();
+        }
+        else
+        {
+            buttonForegroundColor = "#191919".ToColor();
+            buttonHoverForegroundColor = "#191919".ToColor();
+
+            buttonHoverBackgroundColor = "#09000000".ToColor();
+        }
+        res["WindowCaptionForeground"] = buttonForegroundColor;
+
+        window.AppWindow.TitleBar.ButtonForegroundColor = buttonForegroundColor;
+        window.AppWindow.TitleBar.ButtonHoverForegroundColor = buttonHoverForegroundColor;
+
+        window.AppWindow.TitleBar.ButtonHoverBackgroundColor = buttonHoverBackgroundColor;
     }
 }
--- KubeNavigator/Windows/MainWindow.xaml.cs
+++ KubeNavigator/Windows/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
+using CommunityToolkit.WinUI.Helpers;
 using KubeNavigator.Pages;
 using KubeNavigator.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.UI;
 
 namespace KubeNavigator.Windows;
 
@@ -16,7 +18,48 @@ public sealed partial class MainWindow : Window
         ViewModel = viewModel;
         ExtendsContentIntoTitleBar = true;
         SetTitleBar(AppTitleBar);
+        AppTitleBar.Loaded += AppTitleBar_Loaded;
+        AppTitleBar.ActualThemeChanged += AppTitleBar_ActualThemeChanged;
 
         RootFrame.Navigate(typeof(RootPage), viewModel);
+    }
+
+    private void AppTitleBar_Loaded(object sender, RoutedEventArgs e)
+    {
+        ApplySystemThemeToCaptionButtons(this);
+    }
+
+    private void AppTitleBar_ActualThemeChanged(FrameworkElement sender, object args)
+    {
+        ApplySystemThemeToCaptionButtons(this);
+    }
+
+    public void ApplySystemThemeToCaptionButtons(Window window)
+    {
+        var res = Application.Current.Resources;
+        Color buttonForegroundColor;
+        Color buttonHoverForegroundColor;
+
+        Color buttonHoverBackgroundColor;
+        if (ViewModel.App.ThemeManager.GetEffectiveTheme() == "dark")
+        {
+            buttonForegroundColor = "#FFFFFF".ToColor();
+            buttonHoverForegroundColor = "#FFFFFF".ToColor();
+
+            buttonHoverBackgroundColor = "#0FFFFFFF".ToColor();
+        }
+        else
+        {
+            buttonForegroundColor = "#191919".ToColor();
+            buttonHoverForegroundColor = "#191919".ToColor();
+
+            buttonHoverBackgroundColor = "#09000000".ToColor();
+        }
+        res["WindowCaptionForeground"] = buttonForegroundColor;
+
+        window.AppWindow.TitleBar.ButtonForegroundColor = buttonForegroundColor;
+        window.AppWindow.TitleBar.ButtonHoverForegroundColor = buttonHoverForegroundColor;
+
+        window.AppWindow.TitleBar.ButtonHoverBackgroundColor = buttonHoverBackgroundColor;
     }
 }

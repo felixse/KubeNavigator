081bf4ed360572ba757f9f199fb6f78c6cbfaaae
Felix Seidl <me@felixse.dev>
2026-01-01 14:29:41 +0100

    use markdown for delete dialog

--- KubeNavigator/Views/ConfirmDeletionDialog.xaml.cs
+++ KubeNavigator/Views/ConfirmDeletionDialog.xaml.cs
@@ -5,18 +5,18 @@ namespace KubeNavigator.Views;
 
 public sealed partial class ConfirmDeletionDialog : ContentDialog
 {
+    public string Body { get; private set; }
+
     public ConfirmDeletionDialog(ResourceType resourceType, string resourceName, string clusterName)
     {
         this.InitializeComponent();
 
         Title = "Please Confirm";
-        //var content = $"Delete {resourceType.SingularDisplayName} \b{resourceName}\b0 in the cluster \b{clusterName}\b0?";
+        Body =
+            $"Delete {resourceType.SingularDisplayName} **{resourceName}** in the cluster **{clusterName}**?";
 
         PrimaryButtonText = "Delete";
         CloseButtonText = "Cancel";
         DefaultButton = ContentDialogButton.Primary;
-        ResourceType.Text = resourceType.SingularDisplayName;
-        ResourceName.Text = resourceName;
-        ClusterName.Text = clusterName;
     }
 }

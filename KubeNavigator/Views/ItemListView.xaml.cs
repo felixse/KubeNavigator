b1e58fa70219894b37af4b6f1b9a9c8ca6d3ce23
Felix Seidl <me@felixse.dev>
2026-04-16 22:09:05 +0200

    add horizontal scroll support to tables

--- KubeNavigator/Views/ItemListView.xaml.cs
+++ KubeNavigator/Views/ItemListView.xaml.cs
@@ -1,4 +1,5 @@
 using KubeNavigator.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace KubeNavigator.Views;
@@ -7,8 +8,42 @@ public sealed partial class ItemListView : UserControl
 {
     public ListViewModel? ViewModel { get; set; }
 
+    private double _headerWidth;
+
     public ItemListView()
     {
         this.InitializeComponent();
+    }
+
+    private void HeaderContent_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        _headerWidth = e.NewSize.Width;
+        ApplyWidthToAllContainers();
+    }
+
+    private void ItemsListView_ContainerContentChanging(
+        ListViewBase sender,
+        ContainerContentChangingEventArgs args)
+    {
+        if (_headerWidth > 0)
+        {
+            args.ItemContainer.Width = _headerWidth;
+        }
+    }
+
+    private void ApplyWidthToAllContainers()
+    {
+        if (_headerWidth <= 0 || ItemsListView.Items is null)
+        {
+            return;
+        }
+
+        for (var i = 0; i < ItemsListView.Items.Count; i++)
+        {
+            if (ItemsListView.ContainerFromIndex(i) is ListViewItem container)
+            {
+                container.Width = _headerWidth;
+            }
+        }
     }
 }
